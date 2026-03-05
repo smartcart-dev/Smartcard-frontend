b815438b01e9881c055c1a7a48d5fce1a5b891e8
Naviikr <naviikr4@gmail.com>
2026-03-05 17:06:04 +0530

    Change in environment

--- web/src/clients/demo/environment.demo.ts
+++ web/src/clients/demo/environment.demo.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   client: 'demo',
   brandName: 'D-Mart Business',
-  apiBaseUrl: 'http://localhost:3000',
+  apiBaseUrl: 'http://localhost:3000/api',
   themeColor: '#1e88e5',
 };
 
